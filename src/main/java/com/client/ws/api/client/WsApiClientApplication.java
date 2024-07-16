a9e2a75e10ff0d099e19ef4ab83e2e44c7d3fd4d
matheusvidal <matheusvidal140@gmail.com>
2024-07-16 17:16:56 -0300

    First commit

---
+++ src/main/java/com/client/ws/api/client/WsApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.client.ws.api.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsApiClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client/ws/api/client/WsApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.ws.api.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
